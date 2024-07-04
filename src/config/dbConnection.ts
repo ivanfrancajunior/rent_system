472e3c253d4d6151f5c97c4d14be1e039312abed
ivan franca <contato.ivanfrancajr@gmail.com>
2024-07-04 13:35:23 -0300

    feat/ config database

---
+++ src/config/dbConnection.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI!;
+
+const connection = async () => {
+  try {
+    const conn = await mongoose.connect(MONGO_URI);
+
+    if (conn) return console.log("database connection");
+
+    return conn;
+  } catch (error) {
+    console.log("ERROR: ", error);
+  }
+};
+connection();
+
+export default connection;
