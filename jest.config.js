9cbec152b65c0a19276c2a030832c9a11068f398
ivan franca <contato.ivanfrancajr@gmail.com>
2024-07-12 12:28:31 -0300

    feat/ config jest and supertest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: "node",
+  testMatch: ["**/*.spec.ts|**/*.test.ts"],
+  verbose: true,
+  forceExit: true,
+  transform: {
+    "^.+.tsx?$": ["ts-jest", {}],
+  },
+  // clearMocks: true,
+};
---
+++ src/__tests__/dummy.spec.ts
@@ -0,0 +1,5 @@
+describe("config tests", () => {
+  it("should pass", () => {
+    expect(1).toBe(1);
+  });
+});
